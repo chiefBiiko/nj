5cd27a972b752f7ad75b0f07e6e24b0e7abd8b38
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-06 10:41:32 +0100

    purrrr global assignment

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 /* a chrome extension that emulates a console to perform numerical computations in js
 based upon http://www.numericjs.com/ */
 'use strict';
-var nj = {};
-nj.con = document.getElementById('console');
-nj.his = [];  // history array
+var nj = {con: document.getElementById('console'), his: [], ref: document.getElementById('ref')};
 // func 2 process commands and display return val
 function processR(e) {
   var cmd, ret;
@@ -29,16 +27,14 @@ function processR(e) {
 }
 // func that pops up cmd history on arrowkey events
 function commandR(arrow) {
-  var crn;
-  // crn is a unique array of nj.his cmds otherwise not all cmds r retroaccessible
+  var crn;  // crn is a unique array of nj.his cmds otherwise not all cmds r retroaccessible
   (arrow === 'up') ? crn = [...new Set(nj.his.map(x => x[0]))].reverse() : crn = [...new Set(nj.his.map(x => x[0]))];
   if (!nj.prm) {  // nj.prm is temp memory what cmd is on prompt
     nj.con.value += crn[0];
     nj.prm = crn[0];
   } else if (crn[crn.indexOf(nj.prm) + 1]) {  // making sure cmd history item is defined at target index
-    // removing last cmd on prompt before adding new from target index
     nj.con.value = nj.con.value.substr(0, nj.con.value.lastIndexOf('\n') + 1);
-    nj.con.value += crn[crn.indexOf(nj.prm) + 1];
+    nj.con.value += crn[crn.indexOf(nj.prm) + 1]; // rming last cmd on prompt b4 adding new from target
     nj.prm = crn[crn.indexOf(nj.prm) + 1];
   }
   nj.con.setSelectionRange(nj.con.value.length, nj.con.value.length);
@@ -67,6 +63,5 @@ document.getElementById('clear').addEventListener('click', function(e) {
 });
 // toggle btn
 document.getElementById('toggle').addEventListener('click', function(e) {
-  var ref = document.getElementById('ref');
-  (ref.style.display === 'none') ? ref.style.display = 'block' : ref.style.display = 'none'; 
+  (nj.ref.style.display === 'none') ? nj.ref.style.display = 'block' : nj.ref.style.display = 'none'; 
 });
